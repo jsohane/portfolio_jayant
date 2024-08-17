c803605f0d23d650e8b07f805373bbf3bc8ae590
jsohane <jsohane1998@gmail.com>
2024-08-17 11:19:25 -0400

    navbar complete, skeleton for components created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./nav/Nav";
+import About from "./about/About"
+import Skills from "./skills/Skills";
+import Projects from "./projects/Projects";
+import Contact from "./contact.js/Contact";
+import PlayerStats from "./playerStats/PlayerStats";
+
+
 const App = () => {
   return (
-  <>
-  <Nav/>
-  </>
-  )
-}
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<About/>} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <PlayerStats />
+    </Router>
+  );
+};
 
 export default App;
